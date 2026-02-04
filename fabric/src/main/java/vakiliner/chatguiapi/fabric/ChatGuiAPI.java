1bf23ab254b0b4e95e49e1319af325d6b19a500e
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-02-04 19:19:24 +0300

    Add entrypoints

---
+++ fabric/src/main/java/vakiliner/chatguiapi/fabric/ChatGuiAPI.java
@@ -0,0 +1,8 @@
+package vakiliner.chatguiapi.fabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChatGuiAPI implements ModInitializer {
+	public void onInitialize() {
+	}
+}
---
+++ forge/src/main/java/vakiliner/chatguiapi/forge/ChatGuiAPI.java
@@ -0,0 +1,9 @@
+package vakiliner.chatguiapi.forge;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("chatguiapi")
+public class ChatGuiAPI {
+	public ChatGuiAPI() {
+	}
+}
