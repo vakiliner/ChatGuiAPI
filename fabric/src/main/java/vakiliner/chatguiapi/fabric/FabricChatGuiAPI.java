c5e4e4220ca1919d3673c1d05afe139add619de9
VAKiliner <119078586+vakiliner@users.noreply.github.com>
2026-08-18 17:37:57 +0300

    Init

---
+++ fabric/src/main/java/vakiliner/chatguiapi/fabric/FabricChatGuiAPI.java
@@ -0,0 +1,9 @@
+package vakiliner.chatguiapi.fabric;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FabricChatGuiAPI implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
